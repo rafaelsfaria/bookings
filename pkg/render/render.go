1d14208ea1214243586e4c5c9cc7ebe8587b79f4
Rafael Faria <rafaelsanfaria@gmail.com>
2023-09-09 13:46:08 -0300

    add csrf to template data

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"path/filepath"
 
+	"github.com/justinas/nosurf"
 	"github.com/rafaelsfaria/bookings/pkg/config"
 	"github.com/rafaelsfaria/bookings/pkg/models"
 )
@@ -28,11 +29,12 @@ func RenderTemplateSingle(w http.ResponseWriter, tmpl string) {
 	}
 }
 
-func AddDefaultData(td *models.TemplateData) *models.TemplateData {
+func AddDefaultData(td *models.TemplateData, r *http.Request) *models.TemplateData {
+	td.CSRFToken = nosurf.Token(r)
 	return td
 }
 
-func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData) {
+func RenderTemplate(w http.ResponseWriter, r *http.Request, tmpl string, td *models.TemplateData) {
 	// create template cache
 	var tc map[string]*template.Template
 	if app.UseCache {
@@ -48,7 +50,7 @@ func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData)
 	}
 
 	buf := new(bytes.Buffer)
-	td = AddDefaultData(td)
+	td = AddDefaultData(td, r)
 	_ = t.Execute(buf, td)
 
 	// render template
