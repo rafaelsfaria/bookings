d90e6e8ea936a7f78e32f67ef8bb35b719143b61
Rafael Faria <rafaelsanfaria@gmail.com>
2023-09-19 21:32:52 -0300

    adds form required and email validation

--- internal/forms/forms.go
+++ internal/forms/forms.go
@@ -1,8 +1,12 @@
 package forms
 
 import (
+	"fmt"
 	"net/http"
 	"net/url"
+	"strings"
+
+	"github.com/asaskevich/govalidator"
 )
 
 // Form simple form struct
@@ -23,15 +27,39 @@ func New(data url.Values) *Form {
 func (f *Form) Has(field string, r *http.Request) bool {
 	x := r.Form.Get(field)
 
-	if x == "" {
-		f.Errors.Add(field, "This field cannot be empty")
+	return x != ""
+}
+
+// Valid checks true if there are no errors
+func (f *Form) Valid() bool {
+	return len(f.Errors) == 0
+}
+
+// Required checks for required fields
+func (f *Form) Required(fields ...string) {
+	for _, field := range fields {
+		value := f.Get(field)
+		if strings.TrimSpace(value) == "" {
+			f.Errors.Add(field, "This field cannot be blank")
+		}
+	}
+}
+
+// MinLength checks for string minimum length
+func (f *Form) MinLength(field string, length int, r *http.Request) bool {
+	x := f.Get(field)
+
+	if len(x) < length {
+		f.Errors.Add(field, fmt.Sprintf("This field must be at least %d characters long", length))
 		return false
 	}
 
 	return true
 }
 
-// Valid checks true if there are no errors
-func (f *Form) Valid() bool {
-	return len(f.Errors) == 0
+// IsEmail checks for valid email address
+func (f *Form) IsEmail(field string) {
+	if !govalidator.IsEmail(f.Get(field)) {
+		f.Errors.Add(field, "Invalid email address")
+	}
 }
